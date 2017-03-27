b09c7475b6fa60a32dd31f3dfb7996cefee69169
Marina Corona <marinaspam1234@gmail.com>
2017-03-27 17:26:44 -0600

    Songs exist

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'unicorn'
+gem 'factory_girl_rails'
+gem "database_cleaner"
+gem "faker"
 
 group :development, :test do
   gem 'byebug'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|

eca98e57b20b4f4fad746580f968d0803c279346
Marina Corona <marinaspam1234@gmail.com>
2017-03-27 14:18:42 -0600

    First test passing

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,23 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def new
   end
+
+  def create
+    @artist = Artist.create(artist_params)
+    redirect_to @artist
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+
+    def artist_params
+      params.require(:artist).permit(:name, :image_path)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-    resources :artists, only: [:index, :new]
+    resources :artists, only: [:index, :new, :create, :show]
 
 end
